e287cb7919aba12a5c88fd272a1ec92332535f0c
Nathan McNeill <nmcneill@gmail.com>
2016-06-13 17:25:11 -0500

    Initial Commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
 $(document).ready(function() {
-  var number = 100;
+  $("#form").on("submit", function(e) {
+    e.preventDefault();
+    $("#list").empty();
+    var num = $("#number").val();
+    fizzBuzz(num);
+    $("#number").val("").focus();
 
-  for (i = 1; i < number; i++) {
-    if (i % 3 === 0) {
-      if (i % 5 === 0) {
-        $("#list").append("<li>FizzBuzz</li>");
+  });
+
+});
+
+function fizzBuzz(num) {
+  num = parseInt(num);
+  if (isNaN(num)) {
+    alert("stupid! You must enter a number");
+  } else {
+    for (i = 1; i <= num; i++) {
+      if (i % 3 === 0) {
+        if (i % 5 === 0) {
+          $("#list").append("<li>FizzBuzz</li>");
+        } else {
+          $("#list").append("<li>Fizz</li>");
+        }
+      } else if (i % 5 === 0) {
+        $("#list").append("<li>Buzz</li>");
       } else {
-        $("#list").append("<li>Fizz</li>");
+        $("#list").append("<li>" + i + "</li>");
       }
-    } else if (i % 5 === 0) {
-      $("#list").append("<li>Buzz</li>");
-    } else {
-      $("#list").append("<li>" + i + "</li>");
     }
   }
-});
+}
